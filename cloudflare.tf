793573068641cd69077dec0ca887dea406a6280d
hamidreza-ygh <hr.yaghoubi@gmail.com>
2024-05-29 17:05:54 +0200

    Adding DNS record resource

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,11 +1,10 @@
 data "exoscale_nlb" "traefik_nlb" {
-  depends_on = [ helm_release.traefik ]
   zone = local.zone
   name = "traefik-nlb"
 }
 
 resource "cloudflare_record" "tenant_todo_life" {
-  depends_on = [ data.exoscale_nlb.traefik_nlb.ip_address ]
+  depends_on = [ data.exoscale_nlb.traefik_nlb ]
   zone_id = var.cloudflare_domain_zone_id
   name = "${var.app_name}.tenantodo.life"
   value = data.exoscale_nlb.traefik_nlb.ip_address
